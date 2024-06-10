1460ed4c64ad683ee105856f3d7787b8c1705b8c
ozkanjanberk <ozkanjanberk@gmail.com>
2024-06-10 15:31:20 +0300

    Task completed.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,33 @@
 package org.example;
 
 
+import java.util.LinkedList;
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
 
+    public static boolean checkForPalindrome(String input) {
+        String clean = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        StringBuilder reversed = new StringBuilder(clean).reverse();
+
+        return clean.contentEquals(reversed);
+    }
+
+    public static String convertDecimalToBinary(int number) {
+        LinkedList<Integer> binaryList = new LinkedList<>();
+        while (number > 0) {
+            int remaining = number % 2;
+            binaryList.addFirst(remaining);
+            number = number / 2;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int digit : binaryList) {
+            sb.append(digit);
+        }
+        return sb.toString();
+    }
 }
